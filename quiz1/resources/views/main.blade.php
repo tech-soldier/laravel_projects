15da7fdbb6d2e87cdcdd3b14e0f29ed925706a9b
inna_soldier <inna.soldier@gmail.com>
2020-06-30 13:53:05 -0500

    quiz1: added main html template

---
+++ quiz1/resources/views/main.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="all">
+    <title>{{ title }}</title>
+</head>
+<body>
+<nav class="menu">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/support">Support</a>
+    <a href="/services">Services</a>
+    <a href="/contact">Contact</a>
+</nav>
+
+<div id="wrapper">
+    <div id="left">
+        <h1>{{ title }}</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non neque sed lacus dictum eleifend eget porttitor ipsum.
+            Aliquam et orci a nulla convallis posuere. Nam feugiat lorem non commodo tincidunt. Praesent fermentum, nunc sit amet
+            auctor ultricies, leo enim pharetra ex, quis mattis mauris mauris sollicitudin elit. Nunc libero magna, pellentesque vel
+            blandit eget, malesuada sed ex. Fusce suscipit libero quis sollicitudin aliquam. Integer eu convallis urna.
+            Praesent tristique ligula mattis vestibulum varius.</p>
+        <h2>Header Level 2</h2>
+        <p>Praesent sit amet purus viverra, placerat diam ut, vestibulum purus. Donec non mi non purus elementum vehicula.
+            Mauris egestas efficitur diam, a vestibulum lectus imperdiet pulvinar. Quisque mollis ac nulla id porttitor.
+            Aliquam gravida et arcu a blandit. Praesent mi lorem, pellentesque non lectus nec, consectetur ornare ex.
+            Aenean sit amet felis rutrum, efficitur ligula ut, posuere orci.</p>
+        <h3>Header Level 3</h3>
+        <ul>
+            <li>One thing</li>
+            <li>Another thing</li>
+            <li>One more thing</li>
+            <li>And one more</li>
+            <li>Last one, I promise</li>
+        </ul>
+    </div>
+</div>
+<div class="footer">
+    <p><a href="delete_cookies.php">DELETE COOKIES</a></p>
+</div>
+</body>
+</html>
