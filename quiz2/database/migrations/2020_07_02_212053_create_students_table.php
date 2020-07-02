857a0523ca492c93562323cd080c3335770c1760
inna_soldier <inna.soldier@gmail.com>
2020-07-02 16:43:34 -0500

    quiz2: created model, controller, migrations

---
+++ quiz2/database/migrations/2020_07_02_212053_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_number');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('image', 255);
+            $table->string('email', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
